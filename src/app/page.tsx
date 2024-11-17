ee7e0665858949c0230574be60f77882298db97e
Scan <scan@scanash.com>
2024-11-16 16:51:33 -0900

    changed github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
       </main>
       <footer className="mt-8 text-center text-sm text-gray-500">
         <a
-          href="https://github.com/t3dotgg/quickpic"
+          href="https://github.com/scanash00/quickpic-main"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:underline"
